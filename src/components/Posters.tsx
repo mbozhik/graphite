07743b4cf8b626678a2639aaa248d28c739c18c8
max <bozzhik@ya.ru>
2024-03-01 16:34:13 +0200

    extract `PostersData` to separate file

--- src/components/Posters.tsx
+++ src/components/Posters.tsx
@@ -5,24 +5,7 @@ import 'swiper/css'
 import 'swiper/css/pagination'
 import {Pagination, Autoplay} from 'swiper/modules'
 
-const postersData = [
-  {
-    link: 'https://widget.afisha.yandex.ru/w/sessions/NjM5MDh8NDY5MDE1fDU3NzMzOTR8MTcwOTkxNzIwMDAwMA==?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56',
-    imageUrl: '/posters/1.jpg',
-  },
-  {
-    link: 'https://moscow.qtickets.events/95719-komsomolsk-grafit-9-marta',
-    imageUrl: '/posters/2.jpg',
-  },
-  {
-    link: 'https://widget.afisha.yandex.ru/w/sessions/NjM5MDh8NDcxOTIyfDU3NzMzOTR8MTcxMDUyMjAwMDAwMA==?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56',
-    imageUrl: '/posters/3.jpg',
-  },
-  {
-    link: 'https://widget.afisha.yandex.ru/w/sessions/NjM5MDh8NDcxOTAwfDU3NzMzOTR8MTcxMzYzMjQwMDAwMA==?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56',
-    imageUrl: '/posters/4.jpg',
-  },
-]
+import {postersData} from './PostersData'
 
 export default function PostersSlider() {
   const [isMobile, setIsMobile] = useState(false)
---
+++ src/components/PostersData.tsx
@@ -0,0 +1,23 @@
+interface Poster {
+  link: string
+  imageUrl: string
+}
+
+export const postersData: Poster[] = [
+  {
+    link: 'https://widget.afisha.yandex.ru/w/sessions/NjM5MDh8NDY5MDE1fDU3NzMzOTR8MTcwOTkxNzIwMDAwMA==?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56',
+    imageUrl: '/posters/1.jpg',
+  },
+  {
+    link: 'https://moscow.qtickets.events/95719-komsomolsk-grafit-9-marta',
+    imageUrl: '/posters/2.jpg',
+  },
+  {
+    link: 'https://widget.afisha.yandex.ru/w/sessions/NjM5MDh8NDcxOTIyfDU3NzMzOTR8MTcxMDUyMjAwMDAwMA==?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56',
+    imageUrl: '/posters/3.jpg',
+  },
+  {
+    link: 'https://widget.afisha.yandex.ru/w/sessions/NjM5MDh8NDcxOTAwfDU3NzMzOTR8MTcxMzYzMjQwMDAwMA==?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56',
+    imageUrl: '/posters/4.jpg',
+  },
+]
