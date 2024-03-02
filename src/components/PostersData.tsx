06b5267cde87ecd64d90ce3dd5e5d9c51bfa7959
max <bozzhik@ya.ru>
2024-03-02 18:05:59 +0300

    add new events in `PostersData`

--- src/components/PostersData.tsx
+++ src/components/PostersData.tsx
@@ -1,7 +1,7 @@
 interface Poster {
   id: number
   date: string
-  link: string
+  link?: string
   image: string
 }
 
@@ -26,8 +26,56 @@ export const postersData: Poster[] = [
   },
   {
     id: 4,
+    date: '23.03',
+    link: 'https://widget.afisha.yandex.ru/w/sessions/ticketsteam-5811@15682947?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56&regionId=213',
+    image: 'kangi_kambulat',
+  },
+  {
+    id: 5,
+    date: '29.03',
+    link: 'https://ticketscloud.com/v1/widgets/common?token=eyJhbGciOiJIUzI1NiIsImlzcyI6InRpY2tldHNjbG91ZC5ydSIsInR5cCI6IkpXVCJ9.eyJwIjoiNjA4MmExMWIzN2E5MWU3YTQ1YzE1MGQwIn0.cjdDHinwC-6T5hF3tuUu3BZGdPU6uSX29YkCSG2BrnA&event=65ae86aee32dffaccf1295be&s=1&r=1&org=6082a11b37a91e7a45c150d0&city=524901&category=592841f8515e35002dead938&tags=592841f8515e35002dead94a%2C%D0%A0%D1%8D%D0%BF%2F%D0%A5%D0%B8%D0%BF-%D1%85%D0%BE%D0%BF&lang=ru',
+    image: 'shleager',
+  },
+  {
+    id: 6,
+    date: '30.03',
+    link: 'https://widget.afisha.yandex.ru/w/sessions/NjM5MDh8NDgwMzU3fDU3NzMzOTR8MTcxMTgxODAwMDAwMA==?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56',
+    image: 'maty_a4',
+  },
+  {
+    id: 7,
+    date: '31.03',
+    link: 'https://afisha.yandex.ru/moscow/concert/playingtheangel-2024-03-31?source=place',
+    image: 'playing_the_angel',
+  },
+  {
+    id: 8,
+    date: '06.04',
+    link: 'https://widget.afisha.yandex.ru/w/sessions/ticketsteam-5811@16597115?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56&regionId=213',
+    image: 'quok_a4',
+  },
+  {
+    id: 9,
+    date: '13.05',
+    link: 'https://afisha.yandex.ru/moscow/concert/boogie-party-vol-3',
+    image: 'boogie',
+  },
+  {
+    id: 10,
     date: '20.04',
     link: 'https://widget.afisha.yandex.ru/w/sessions/NjM5MDh8NDcxOTAwfDU3NzMzOTR8MTcxMzYzMjQwMDAwMA==?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56',
     image: 'radio_chacha',
+  },
+  {
+    id: 11,
+    date: '24.05',
+    link: 'https://widget.afisha.yandex.ru/w/sessions/NjM5MDh8NDkzMzA3fDYzODQzNjV8MTcxNjU3MDAwMDAwMA==?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56',
+    image: 'temnee',
+  },
+  {
+    id: 12,
+    date: '01.05',
+    link: 'http://slezyfest.ru/',
+    image: 'slezi',
   },
 ]
