dfcf83c2ba13c183e72f0e900f4ac87f9ab757d2
max <bozzhik@ya.ru>
2024-03-02 17:44:03 +0300

    optimize `PostersData`

--- src/components/PostersData.tsx
+++ src/components/PostersData.tsx
@@ -1,23 +1,33 @@
 interface Poster {
+  id: number
+  date: string
   link: string
-  imageUrl: string
+  image: string
 }
 
 export const postersData: Poster[] = [
   {
+    id: 1,
+    date: '8.03',
     link: 'https://widget.afisha.yandex.ru/w/sessions/NjM5MDh8NDY5MDE1fDU3NzMzOTR8MTcwOTkxNzIwMDAwMA==?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56',
-    imageUrl: '/posters/1.jpg',
+    image: 'bahit_kompot',
   },
   {
+    id: 2,
+    date: '9.03',
     link: 'https://moscow.qtickets.events/95719-komsomolsk-grafit-9-marta',
-    imageUrl: '/posters/2.jpg',
+    image: 'komsomolsk',
   },
   {
+    id: 3,
+    date: '15.03',
     link: 'https://widget.afisha.yandex.ru/w/sessions/NjM5MDh8NDcxOTIyfDU3NzMzOTR8MTcxMDUyMjAwMDAwMA==?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56',
-    imageUrl: '/posters/3.jpg',
+    image: 'kirpici',
   },
   {
+    id: 4,
+    date: '20.04',
     link: 'https://widget.afisha.yandex.ru/w/sessions/NjM5MDh8NDcxOTAwfDU3NzMzOTR8MTcxMzYzMjQwMDAwMA==?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56',
-    imageUrl: '/posters/4.jpg',
+    image: 'radio_chacha',
   },
 ]
