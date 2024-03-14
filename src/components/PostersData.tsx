4a0148cb2bdaaa57f8f312091cdfbd2a736ae7e9
bozzhik <bozzhik@ya.ru>
2024-03-14 03:31:48 +0300

    minor refactor `PostersData`

--- src/components/PostersData.tsx
+++ src/components/PostersData.tsx
@@ -1,72 +1,58 @@
 interface Poster {
-  id: number
   date: string
   link?: string
   image: string
-  lazy?: boolean
 }
 
 export const postersData: Poster[] = [
   {
-    id: 3,
+    image: 'kirpici',
     date: '15.03',
     link: 'https://widget.afisha.yandex.ru/w/sessions/ticketsteam-5811@16778850?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56&regionId=213',
-    image: 'kirpici',
-    lazy: false,
   },
   {
-    id: 4,
+    image: 'kangi_kambulat',
     date: '23.03',
     link: 'https://widget.afisha.yandex.ru/w/sessions/ticketsteam-5811@16778617?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56&regionId=213',
-    image: 'kangi_kambulat',
-    lazy: false,
   },
   {
-    id: 5,
+    image: 'shleager',
     date: '29.03',
     link: 'https://ticketscloud.com/v1/widgets/common?token=eyJhbGciOiJIUzI1NiIsImlzcyI6InRpY2tldHNjbG91ZC5ydSIsInR5cCI6IkpXVCJ9.eyJwIjoiNjA4MmExMWIzN2E5MWU3YTQ1YzE1MGQwIn0.cjdDHinwC-6T5hF3tuUu3BZGdPU6uSX29YkCSG2BrnA&event=65ae86aee32dffaccf1295be&s=1&r=1&org=6082a11b37a91e7a45c150d0&city=524901&category=592841f8515e35002dead938&tags=592841f8515e35002dead94a%2C%D0%A0%D1%8D%D0%BF%2F%D0%A5%D0%B8%D0%BF-%D1%85%D0%BE%D0%BF&lang=ru',
-    image: 'shleager',
   },
   {
-    id: 6,
+    image: 'maty_a4',
     date: '30.03',
     link: 'https://widget.afisha.yandex.ru/w/sessions/NjM5MDh8NDgwMzU3fDU3NzMzOTR8MTcxMTgxODAwMDAwMA==?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56',
-    image: 'maty_a4',
   },
   {
-    id: 7,
+    image: 'playing_the_angel',
     date: '31.03',
     link: 'https://vk.com/app51856676_-224604814',
-    image: 'playing_the_angel',
   },
   {
-    id: 8,
+    image: 'quok_a4',
     date: '06.04',
     link: 'https://widget.afisha.yandex.ru/w/sessions/NjM5MDh8NDkzMzAyfDY0MjM3OTV8MTcxMjQzMDAwMDAwMA==?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56',
-    image: 'quok_a4',
   },
   {
-    id: 9,
+    image: 'boogie',
     date: '13.05',
     link: 'https://afisha.yandex.ru/moscow/concert/boogie-party-vol-3',
-    image: 'boogie',
   },
   {
-    id: 10,
+    image: 'radio_chacha',
     date: '20.04',
     link: 'https://widget.afisha.yandex.ru/w/sessions/NjM5MDh8NDcxOTAwfDU3NzMzOTR8MTcxMzYzMjQwMDAwMA==?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56',
-    image: 'radio_chacha',
   },
   {
-    id: 11,
+    image: 'temnee',
     date: '24.05',
     link: 'https://widget.afisha.yandex.ru/w/sessions/NjM5MDh8NDkzMzA3fDYzODQzNjV8MTcxNjU3MDAwMDAwMA==?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56',
-    image: 'temnee',
   },
   {
-    id: 12,
+    image: 'slezi',
     date: '01.05',
     link: 'http://slezyfest.ru/',
-    image: 'slezi',
   },
 ]
