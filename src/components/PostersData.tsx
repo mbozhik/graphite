f2134eba9e656e480add4038793984f1e6110cd6
bozzhik <bozzhik@ya.ru>
2024-03-29 17:02:09 +0300

    edit `current` events

--- src/components/PostersData.tsx
+++ src/components/PostersData.tsx
@@ -6,11 +6,6 @@ interface Poster {
 
 export const postersData: Poster[] = [
   {
-    image: 'kangi_kambulat',
-    date: '23.march',
-    link: 'https://widget.afisha.yandex.ru/w/sessions/ticketsteam-5811@16778617?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56&regionId=213',
-  },
-  {
     image: 'shleager',
     date: '29.march',
     link: 'https://ticketscloud.com/v1/widgets/common?token=eyJhbGciOiJIUzI1NiIsImlzcyI6InRpY2tldHNjbG91ZC5ydSIsInR5cCI6IkpXVCJ9.eyJwIjoiNjA4MmExMWIzN2E5MWU3YTQ1YzE1MGQwIn0.cjdDHinwC-6T5hF3tuUu3BZGdPU6uSX29YkCSG2BrnA&event=65ae86aee32dffaccf1295be&s=1&r=1&org=6082a11b37a91e7a45c150d0&city=524901&category=592841f8515e35002dead938&tags=592841f8515e35002dead94a%2C%D0%A0%D1%8D%D0%BF%2F%D0%A5%D0%B8%D0%BF-%D1%85%D0%BE%D0%BF&lang=ru',
@@ -26,13 +21,8 @@ export const postersData: Poster[] = [
     link: 'https://vk.com/app51856676_-224604814',
   },
   {
-    image: 'quok_a4',
-    date: '06.april',
-    link: 'https://widget.afisha.yandex.ru/w/sessions/NjM5MDh8NDkzMzAyfDY0MjM3OTV8MTcxMjQzMDAwMDAwMA==?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56',
-  },
-  {
     image: 'boogie',
-    date: '13.may',
+    date: '13.april',
     link: 'https://afisha.yandex.ru/moscow/concert/boogie-party-vol-3',
   },
   {
@@ -41,9 +31,19 @@ export const postersData: Poster[] = [
     link: 'https://widget.afisha.yandex.ru/w/sessions/ticketsteam-5811@16955192?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56&regionId=213',
   },
   {
+    image: 'kangi_kambulat',
+    date: '10.may',
+    link: 'https://widget.afisha.yandex.ru/w/sessions/ticketsteam-5811@16778617?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56&regionId=213',
+  },
+  {
     image: 'temnee',
     date: '24.may',
     link: 'https://widget.afisha.yandex.ru/w/sessions/NjM5MDh8NDkzMzA3fDYzODQzNjV8MTcxNjU3MDAwMDAwMA==?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56',
+  },
+  {
+    image: 'quok',
+    date: '31.may',
+    link: 'https://widget.afisha.yandex.ru/w/sessions/NjM5MDh8NDkzMzAyfDY0MjM3OTV8MTcxMjQzMDAwMDAwMA==?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56',
   },
   {
     image: 'slezi',
