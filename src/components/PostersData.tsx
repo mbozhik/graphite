6fc3e52dea6f4491bc8934483abf06fe1908ad6b
bozzhik <bozzhik@ya.ru>
2024-03-29 16:54:55 +0300

    delete `past` events
    kirpici + alblak

--- src/components/PostersData.tsx
+++ src/components/PostersData.tsx
@@ -6,58 +6,48 @@ interface Poster {
 
 export const postersData: Poster[] = [
   {
-    image: 'kirpici',
-    date: '15.03',
-    link: 'https://widget.afisha.yandex.ru/w/sessions/ticketsteam-5811@16778850?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56&regionId=213',
-  },
-  {
-    image: 'alblak',
-    date: '22.03',
-    link: 'https://www.afisha.ru/w/creations/userevent/208/2/a42ade48-08e9-485f-a8ff-0f8db4c20b75/https%253a%252f%252fwww.afisha.ru',
-  },
-  {
     image: 'kangi_kambulat',
-    date: '23.03',
+    date: '23.march',
     link: 'https://widget.afisha.yandex.ru/w/sessions/ticketsteam-5811@16778617?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56&regionId=213',
   },
   {
     image: 'shleager',
-    date: '29.03',
+    date: '29.march',
     link: 'https://ticketscloud.com/v1/widgets/common?token=eyJhbGciOiJIUzI1NiIsImlzcyI6InRpY2tldHNjbG91ZC5ydSIsInR5cCI6IkpXVCJ9.eyJwIjoiNjA4MmExMWIzN2E5MWU3YTQ1YzE1MGQwIn0.cjdDHinwC-6T5hF3tuUu3BZGdPU6uSX29YkCSG2BrnA&event=65ae86aee32dffaccf1295be&s=1&r=1&org=6082a11b37a91e7a45c150d0&city=524901&category=592841f8515e35002dead938&tags=592841f8515e35002dead94a%2C%D0%A0%D1%8D%D0%BF%2F%D0%A5%D0%B8%D0%BF-%D1%85%D0%BE%D0%BF&lang=ru',
   },
   {
     image: 'maty_a4',
-    date: '30.03',
+    date: '30.march',
     link: 'https://widget.afisha.yandex.ru/w/sessions/ticketsteam-5811@16954960?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56&regionId=213',
   },
   {
     image: 'playing_the_angel',
-    date: '31.03',
+    date: '31.march',
     link: 'https://vk.com/app51856676_-224604814',
   },
   {
     image: 'quok_a4',
-    date: '06.04',
+    date: '06.april',
     link: 'https://widget.afisha.yandex.ru/w/sessions/NjM5MDh8NDkzMzAyfDY0MjM3OTV8MTcxMjQzMDAwMDAwMA==?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56',
   },
   {
     image: 'boogie',
-    date: '13.05',
+    date: '13.may',
     link: 'https://afisha.yandex.ru/moscow/concert/boogie-party-vol-3',
   },
   {
     image: 'radio_chacha',
-    date: '20.04',
+    date: '20.april',
     link: 'https://widget.afisha.yandex.ru/w/sessions/ticketsteam-5811@16955192?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56&regionId=213',
   },
   {
     image: 'temnee',
-    date: '24.05',
+    date: '24.may',
     link: 'https://widget.afisha.yandex.ru/w/sessions/NjM5MDh8NDkzMzA3fDYzODQzNjV8MTcxNjU3MDAwMDAwMA==?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56',
   },
   {
     image: 'slezi',
-    date: '01.05',
+    date: '01.may',
     link: 'http://slezyfest.ru/',
   },
 ]
