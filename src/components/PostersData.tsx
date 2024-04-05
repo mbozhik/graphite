d17ddb2b6be07ded41346f66caa8a45ed287a3c6
bozzhik <bozzhik@ya.ru>
2024-04-05 17:34:27 +0300

    delete `past` events

--- src/components/PostersData.tsx
+++ src/components/PostersData.tsx
@@ -6,21 +6,6 @@ interface Poster {
 
 export const postersData: Poster[] = [
   {
-    image: 'shleager',
-    date: '29.march',
-    link: 'https://ticketscloud.com/v1/widgets/common?token=eyJhbGciOiJIUzI1NiIsImlzcyI6InRpY2tldHNjbG91ZC5ydSIsInR5cCI6IkpXVCJ9.eyJwIjoiNjA4MmExMWIzN2E5MWU3YTQ1YzE1MGQwIn0.cjdDHinwC-6T5hF3tuUu3BZGdPU6uSX29YkCSG2BrnA&event=65ae86aee32dffaccf1295be&s=1&r=1&org=6082a11b37a91e7a45c150d0&city=524901&category=592841f8515e35002dead938&tags=592841f8515e35002dead94a%2C%D0%A0%D1%8D%D0%BF%2F%D0%A5%D0%B8%D0%BF-%D1%85%D0%BE%D0%BF&lang=ru',
-  },
-  {
-    image: 'maty_a4',
-    date: '30.march',
-    link: 'https://widget.afisha.yandex.ru/w/sessions/ticketsteam-5811@16954960?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56&regionId=213',
-  },
-  {
-    image: 'playing_the_angel',
-    date: '31.march',
-    link: 'https://vk.com/app51856676_-224604814',
-  },
-  {
     image: 'radio_chacha',
     date: '20.april',
     link: 'https://widget.afisha.yandex.ru/w/sessions/ticketsteam-5811@16955192?clientKey=058374e2-3eea-4d37-ae2f-3e21651b7c56&regionId=213',
