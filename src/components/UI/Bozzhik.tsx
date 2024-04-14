ed2873993366047173ba33b6df8d73df4de0881c
bozzhik <bozzhik@ya.ru>
2024-04-14 23:44:27 +0300

    add `Bozzhik` block with author

---
+++ src/components/UI/Bozzhik.tsx
@@ -0,0 +1,11 @@
+export default function Bozzhik() {
+  return (
+    <footer className="flex items-center justify-between pb-5 group text-white/70">
+      <h1>©️ graphite.house</h1>
+
+      <a className="block uppercase group" href="https://bozzhik.ru" target="_blank">
+        Сайт разработан <span className="underline duration-200 underline-offset-2 text-white/70 group-hover:text-custom-pink">bozzhik.ru</span>
+      </a>
+    </footer>
+  )
+}
