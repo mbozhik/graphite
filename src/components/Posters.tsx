1cae5d76e773d71947e6f4899cbcb240cfc23da8
bozzhik <bozzhik@ya.ru>
2024-04-19 19:00:28 +0300

    install `bozzhik/is-mobile` & refactor `Posters` slider component

--- src/components/Posters.tsx
+++ src/components/Posters.tsx
@@ -1,3 +1,5 @@
+import {isMobile} from '@bozzhik/is-mobile'
+
 import {useEffect, useState} from 'react'
 
 import {Swiper, SwiperSlide} from 'swiper/react'
@@ -8,16 +10,7 @@ import 'swiper/css/pagination'
 import {postersData} from './PostersData'
 
 export default function PostersSlider() {
-  const [isMobile, setIsMobile] = useState(false)
   const [sortedPostersData, setSortedPostersData] = useState([])
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768)
-    }
-
-    handleResize()
-  }, [])
 
   useEffect(() => {
     const sortedData = [...postersData].sort((a, b) => {
@@ -33,21 +26,20 @@ export default function PostersSlider() {
 
   return (
     <div className="my-10 overflow-hidden">
-      {/* prettier-ignore */}
       <Swiper
         className="h-[60vh] xl:h-[65vh] sm:h-fit"
         spaceBetween={30}
         autoplay={{delay: 2000}}
         speed={700}
-        loop={isMobile ? true : false}
+        loop={isMobile ? true : false} // based on @bozzhik/is-mobile
         pagination={{clickable: true}}
         grabCursor={true}
         modules={[Pagination, Autoplay]}
-        slidesPerView={isMobile ? 1 : 3}
+        slidesPerView={isMobile ? 1 : 3} // based on @bozzhik/is-mobile
       >
         {sortedPostersData.map((poster, index) => (
           <SwiperSlide key={index}>
-            <a href={poster.link} target="_blank" title={'link' + index}>
+            <a href={poster.link} className={`${poster.disabled && 'pointer-events-none cursor-none'}`} target="_blank" title={'link' + index}>
               <img className="object-cover duration-500 hover:scale-[102%] s-full" src={`/posters/${poster.image}.jpg`} loading={index < 4 ? 'eager' : 'lazy'} alt={poster.image} />
             </a>
           </SwiperSlide>
