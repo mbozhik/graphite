0bc8c939df4ea980651351984cdc346edf102b61
max <bozzhik@ya.ru>
2024-04-27 16:04:43 +0300

    ~ edit `resizing` of poster images

--- src/components/Posters.tsx
+++ src/components/Posters.tsx
@@ -40,7 +40,7 @@ export default function PostersSlider() {
         {sortedPostersData.map((poster, index) => (
           <SwiperSlide key={index}>
             <a href={poster.link} className={`${poster.disabled && 'pointer-events-none cursor-none'}`} target="_blank" title={'link' + index}>
-              <img className="object-cover duration-500 hover:scale-[102%] s-full" src={`/posters/${poster.image}.jpg`} loading={index < 4 ? 'eager' : 'lazy'} alt={poster.image} />
+              <img className="object-contain duration-500 hover:scale-[102%] s-full" src={`/posters/${poster.image}.jpg`} loading={index < 4 ? 'eager' : 'lazy'} alt={poster.image} />
             </a>
           </SwiperSlide>
         ))}
