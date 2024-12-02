a0d2122bf294acc6375b5f08f1f42de6e8136f2f
maxim <bozzhik@ya.ru>
2024-12-02 18:17:52 +0300

    delete `past` events

--- src/components/PostersData.tsx
+++ src/components/PostersData.tsx
@@ -7,46 +7,6 @@ export type Poster = {
 
 export const postersData: Poster[] = [
   {
-    image: 'soltwine',
-    date: '15.november',
-    link: 'https://ticketscloud.com/v1/widgets/common?token=eyJhbGciOiJIUzI1NiIsImlzcyI6InRpY2tldHNjbG91ZC5ydSIsInR5cCI6IkpXVCJ9.eyJwIjoiNjM3MzllZmE3NGU2Mzc0ZDY5MzYxZTkwIn0.-8jfU8beUezlq8wiAH14gQUd-XrhGGX-Ph12q8RAo_k&event=66d5befd6ccd50d88f62bd73&s=1&clckid=051c3bb5&r=1&org=63739efa74e6374d69361e90&city=524901&category=592841f8515e35002dead938&tags=592841f8515e35002dead939%2CПоп%2C592841f8515e35002dead93a%2CИнди%2C592841f8515e35002dead93b%2CРок%2C592841f8515e35002dead948%2CАльтернатива&clckid=485a6099&lang=ru',
-  },
-  {
-    image: 'drummatix',
-    date: '16.november',
-    link: 'https://madstreambooking.com/drummatix/',
-  },
-  {
-    image: 'south404',
-    date: '17.november',
-    link: 'https://vk.com/app51819233_-210545679?ref=group_widget',
-  },
-  {
-    image: 'bez_obid',
-    date: '22.november',
-    link: 'https://ticketscloud.com/v1/widgets/common?token=eyJhbGciOiJIUzI1NiIsImlzcyI6InRpY2tldHNjbG91ZC5ydSIsInR5cCI6IkpXVCJ9.eyJwIjoiNjViYTc1MzM5ODg2MTgxMGI3YTI1OWNmIn0.qGrtISyqOsUb6PQrwBOTEWrtt79BqsTfiH19ylcdO-U&event=66818c4f7c77cd2efc6bee00&s=1&r=1&org=65ba753398861810b7a259cf&city=524901&category=592841f8515e35002dead938&tags=592841f8515e35002dead93b,Рок&lang=ru',
-  },
-  {
-    image: 'russian_style',
-    date: '23.november',
-    link: 'https://www.russianstyle.fun/',
-  },
-  {
-    image: 'thrill_pill',
-    date: '29.november',
-    link: 'https://moscow.qtickets.events/132657-thrill-pill-2911',
-  },
-  {
-    image: 'elen_lili',
-    date: '30.november',
-    link: 'https://msk.radario.ru/events/2314124/tickets',
-  },
-  {
-    image: 'pepel_nahudi',
-    date: '1.december',
-    link: 'https://moscow.qtickets.events/133376-pepel-nahudi-1-dekabrya',
-  },
-  {
     image: 'cold_carti',
     date: '06.december',
     link: 'https://ticketscloud.com/v1/widgets/common?token=eyJhbGciOiJIUzI1NiIsImlzcyI6InRpY2tldHNjbG91ZC5ydSIsInR5cCI6IkpXVCJ9.eyJwIjoiNjA4MmExMWIzN2E5MWU3YTQ1YzE1MGQwIn0.cjdDHinwC-6T5hF3tuUu3BZGdPU6uSX29YkCSG2BrnA&event=66aa73f2c7380d0a1b8e993d&s=1&r=1&org=6082a11b37a91e7a45c150d0&city=524901&category=592841f8515e35002dead938&tags=592841f8515e35002dead94a%2CРэп%2FХип-хоп&lang=ru',
