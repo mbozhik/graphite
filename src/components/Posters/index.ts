44c3a39c401255e9a66de2e9920be54f4ad567cd
maxim <bozzhik@ya.ru>
2025-06-05 20:07:08 +0300

    delete `past` events and add `new` events

--- src/components/Posters/index.ts
+++ src/components/Posters/index.ts
@@ -4,28 +4,39 @@ export type Poster = {
   image: string
   date: string
   link?: string
+  area?: string
   disabled?: boolean
 }
 
 export const POSTERS_DATA: Poster[] = [
   {
-    image: 'mafanya',
-    date: '26.january',
-    link: 'https://vk.com/app52691227',
+    image: 'mostick',
+    date: '06-07.june',
+    link: 'https://mostick.club',
+    area: 'ХОЛЛ',
   },
   {
-    image: 'pokrov',
-    date: '21.march',
-    link: 'https://ticketscloud.com/v1/widgets/common?token=eyJhbGciOiJIUzI1NiIsImlzcyI6InRpY2tldHNjbG91ZC5ydSIsInR5cCI6IkpXVCJ9.eyJwIjoiNjY2N2U4ZGY5YzhiNWRlNzUxNzIyNGRiIn0.sYld6aMxXr6DhcqMDvVVooQjeLMGz-hw_AOzBFeF7w0&event=6765618e9f5b054c77567c80&s=1&r=1&org=6667e8df9c8b5de7517224db&city=524901&category=592841f8515e35002dead938&tags=592841f8515e35002dead939%2CПоп&lang=ru',
+    image: 'jollyear',
+    date: '07.june',
+    link: 'https://moscow.qtickets.events/168026-jollyear-place-street-summer-day-open-air-scally-milano-polka-baby-cute?clckid=628a6037',
+    area: 'ДВОР',
   },
   {
-    image: 'srub',
-    date: '22.march',
-    link: 'https://moscow.qtickets.events/144610-srub-22-marta',
+    image: 'uppercuts',
+    date: '05.july',
+    link: 'https://uppercuts.ticketscloud.org',
+    area: 'ДВОР',
   },
   {
-    image: 'beshenstvo',
-    date: '26.april',
-    link: 'https://ticketscloud.com/v1/widgets/common?event=676d5d78505caba96bb8ab4b&token=eyJhbGciOiJIUzI1NiIsImlzcyI6InRpY2tldHNjbG91ZC5ydSIsInR5cCI6IkpXVCJ9.eyJwYXJ0bmVyIjoiNjc2YWQwYjc5YzQ5ZDU2MTI1ZjIxNzRlIn0.XRZLmyYXG1E_KoHCj4oiqLpbfAY5UzrXNGjkLEii4Z8&partner=676ad0b79c49d56125f2174e&m=mobile&r=1&org=676ad0b79c49d56125f2174e&city=524901&category=592841f8515e35002dead938&tags=592841f8515e35002dead93b%2CРок%2C592841f8515e35002dead942%2CМеталл%2C592841f8515e35002dead948%2CАльтернатива%2C592e90d7d352860016e476c3%2CШоу+-+программа&lang=ru',
+    image: 'blaash',
+    date: '19.july',
+    link: 'https://blaash-wav.ru',
+    area: 'ДВОР',
+  },
+  {
+    image: 'russianstylefest',
+    date: '26.july',
+    link: 'https://russianstyle.fun',
+    area: 'ДВОР',
   },
 ]
