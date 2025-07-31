39407f669162c135a300608a41efccf8b2190a6f
maxim <bozzhik@gmail.com>
2025-07-31 22:40:27 +0700

    🎶 add `new` events

--- src/components/Posters/index.ts
+++ src/components/Posters/index.ts
@@ -10,9 +10,21 @@ export type Poster = {
 
 export const POSTERS_DATA: Poster[] = [
   {
-    image: 'minutnahistoria',
-    date: '09.august',
-    link: 'https://vk.com/minutehistorfest?ref=group_menu&w=app5732604_-230146808',
-    area: 'ВСЯ ТЕРРИТОРИЯ',
+    image: 'bastard',
+    date: '08.august',
+    link: 'https://madstreambooking.com/theodorbastard/?utm_source=yandex&utm_medium=cpc&utm_campaign=S_TB_SU_25_MSK&utm_content=Москва&utm_term=теодор%20бастард%20билеты&yclid=5872524981981413375',
+    area: 'ДВОР',
   },
+  {
+    image: 'dragni',
+    date: '10.august',
+    link: 'https://dragnitour.ru',
+    area: 'ДВОР',
+  },
+  // {
+  //   image: 'minutnahistoria',
+  //   date: '09.august',
+  //   link: 'https://vk.com/minutehistorfest?ref=group_menu&w=app5732604_-230146808',
+  //   area: 'ВСЯ ТЕРРИТОРИЯ',
+  // },
 ]
